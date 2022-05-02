9d1b3a10d0aeee67db88d689bb8bc2b5d3d440a6
Dionicio Gonzalez <dionicio.gonzalez@healthtrustpg.com>
2022-05-02 16:44:52 -0400

    Added Column Filters, Excel and PDF Export, Fix to Member Portal links

--- Automation/SupplyChainDisruptionImpactAssessmentExport.ps1
+++ Automation/SupplyChainDisruptionImpactAssessmentExport.ps1
@@ -6,8 +6,11 @@ $SiteURL = "https://hcahealthcare.sharepoint.com/sites/HTPS-healthtrustsupplycha
 $ListName = "Issue Tracker"
 $SelectedFields = @("Priority","DateReported", "Category", "Contract_x0020_No", "Product_x0020_Impacted", "Communication_x0020_Link", "Description", "Issue_x0020_Type", "Sourcing_x0020_Option", "Resources", "Cross_x0020_Reference_x0020_Prod") 
 $CSVPath = "\\CORPDPT08\HPGShare\Common\SCDMemberView\SupplyChainDisruptionImpactAssessment.csv"
+$CSVPath_Plain = "\\CORPDPT08\HPGShare\Common\SCDMemberView\SupplyChainDisruptionImpactAssessment_Plain.csv"
 $JSONPath = "\\CORPDPT08\HPGShare\Common\SCDMemberView\SupplyChainDisruptionImpactAssessment.json"
+$JSONPath_Plain = "\\CORPDPT08\HPGShare\Common\SCDMemberView\SupplyChainDisruptionImpactAssessment_Plain.json"
 $FormattedPath = "\\CORPDPT08\HPGShare\Common\SCDMemberView\Publish\lib\source.js"
+$FormattedPath_Plain = "\\CORPDPT08\HPGShare\Common\SCDMemberView\Publish\lib\source_plain.js"
 $ResourcesPath = "/Shared Documents/Resources"
 $ResourcesDownloadPath = "\\CORPDPT08\HPGShare\Common\SCDMemberView\Publish\Resources"
  
@@ -19,26 +22,56 @@ $ListItems = Get-PnPListItem -List $ListName -PageSize 500 -Fields $SelectedFiel
  
 #Iterate through each item and extract data
 $ListDataColl = @()
+$ListDataColl_Plain = @()
+
 $ListItems | ForEach-Object {
     $ListData = New-Object PSObject
+    $ListData_Plain = New-Object PSObject
     #Get the Field Values of the item as text
     $ListItem  = Get-PnPProperty -ClientObject $_ -Property FieldValuesAsHTML 
+    $ListItem_Plain  = Get-PnPProperty -ClientObject $_ -Property FieldValuesAsText 
+    $ContractID = ''
+    $ItemValue = ''
+
     ForEach($Field in $SelectedFields)
     {
-        $ListData | Add-Member Noteproperty $Field $ListItem[$Field]
+        If($Field -eq 'Contract_x0020_No') 
+        {
+            $ContractID = $ListItem_Plain[$Field]
+        }
+
+        If(($Field -eq 'Resources') -or ($Field -eq 'Cross_x0020_Reference_x0020_Prod'))
+        {
+            $ContractURL = 'href="https://members.healthtrustpg.com/contracts/' + $ContractID + '"'
+            $ItemValue = $ListItem[$Field]
+            $ItemValue = $ItemValue -replace 'href=\"https\u0026#58;//members.healthtrustpg.com/-/media/.*\"', $ContractURL
+        }
+        Else
+        {
+            $ItemValue = $ListItem[$Field]
+        }
+
+        $ListData | Add-Member Noteproperty $Field $ItemValue
+        $ListData_Plain | Add-Member Noteproperty $Field $ListItem_Plain[$Field]
     }  
     $ListDataColl += $ListData 
+    $ListDataColl_Plain += $ListData_Plain
 }
 
 #Export data to CSV
 $ListDataColl
+$ListDataColl_Plain
 $ListDataColl | Export-CSV $CSVPath -NoTypeInformation
+$ListDataColl_Plain | Export-CSV $CSVPath_Plain -NoTypeInformation
 
 #Prepare JSON
 Clear-Content -Path $JSONPath -Force
+Clear-Content -Path $JSONPath_Plain -Force
 
-import-csv $CSVPath | ConvertTo-Json | Add-Content -Path $JSONPath
+Import-CSV $CSVPath | ConvertTo-Json | Add-Content -Path $JSONPath
+Import-CSV $CSVPath_Plain | ConvertTo-Json | Add-Content -Path $JSONPath_Plain
 
+#Clean up
 $lookupTable = @{
     '\?' = ''
     'u003ca href' = 'u003ca target=\"_blank\" href'
@@ -60,10 +93,31 @@ Get-Content -Path $JSONPath | ForEach-Object {
             $line = $line -replace $_.Key, $_.Value
         }
     }
+
    $line
 } | Set-Content -Path $FormattedPath
 
+$lookupTable_Plain = @{
+    '\?' = ''
+    '\\u0026' = ''
+}
+
+Get-Content -Path $JSONPath_Plain | ForEach-Object {
+    $line = $_
+
+    $lookupTable_Plain.GetEnumerator() | ForEach-Object {
+        if ($line -match $_.Key)
+        {
+            $line = $line -replace $_.Key, $_.Value
+        }
+    }
+
+   $line
+} | Set-Content -Path $FormattedPath_Plain
+
+# format json
 "var json_data = " + (Get-Content $FormattedPath -Raw) | Set-Content $FormattedPath
+"var json_data_plain = " + (Get-Content $FormattedPath_Plain -Raw) | Set-Content $FormattedPath_Plain
 
 ### download resources
 $CurrentResources = $ResourcesDownloadPath + "\*.*"
